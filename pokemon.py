4df5bc46877e9a89dd45ac51823b94b7dda16ebb
Matthew <mattdasilva23@hotmail.co.uk>
2021-08-29 13:17:30 +0100

    big pokemon.py changes (check prev commit for todo list)

--- pokemon.py
+++ pokemon.py
@@ -1,5 +1,4 @@
 
-
 ITEMS = {
     'Macho Brace': lambda evs: evs * 2,
     'Power Weight': lambda evs: evs + EvSet(hp=4),
@@ -19,20 +18,24 @@ class EvSet(object):
     MAX_STAT = 255
     MAX_EV = 510
 
+    operator = ''
+
     @staticmethod
     def label(stat):
         return EvSet.LABELS[EvSet.STATS.index(stat)]
 
-    def __init__(self, hp=0, attack=0, defense=0, special_attack=0,
-                 special_defense=0, speed=0):
+    def __init__(self, hp=0, attack=0, defense=0, special_attack=0, special_defense=0, speed=0):
         self.hp = int(hp)
         self.attack = int(attack)
         self.defense = int(defense)
         self.special_attack = int(special_attack)
         self.special_defense = int(special_defense)
         self.speed = int(speed)
+        # print('\n\tEVS init\n\t-----------')
+        # print(self)
 
     def __iadd__(self, other):
+        EvSet.operator = '+'
         for stat in EvSet.STATS:
             self.__dict__[stat] += other.__dict__[stat]
         return self
@@ -40,6 +43,17 @@ class EvSet(object):
     def __add__(self, other):
         evs = self.clone()
         evs += other
+        return evs
+
+    def __isub__(self, other):
+        EvSet.operator = '-'
+        for stat in EvSet.STATS:
+            self.__dict__[stat] -= other.__dict__[stat]
+        return self
+
+    def __sub__(self, other):
+        evs = self.clone()
+        evs -= other
         return evs
 
     def __imul__(self, integer):
@@ -54,13 +68,11 @@ class EvSet(object):
         return evs
 
     def __str__(self):
-        ev_string = ['+%d %s' % (ev, EvSet.label(stat)) for stat, ev in self.to_dict().items() if ev > 0]
+        ev_string = [EvSet.operator + str(ev) + ' ' + str(EvSet.label(stat)) for stat, ev in self.to_dict().items() if ev > 0]
         return ', '.join(ev_string)
 
     def verbose(self):
-        ev_string = ['%s: %d' % (EvSet.label(stat), ev) for stat, ev in self.evs.items()]
-        if not len(ev_string):
-            return 'No EVs'
+        ev_string = [str(ev).zfill(3) + ' | +' + format(ev/4, '.2f') + ' | ' + str(EvSet.label(stat)) for stat, ev in self.to_dict().items()]
         return '\n'.join(ev_string)
 
     def clone(self):
@@ -73,13 +85,14 @@ class EvSet(object):
         return dict
 
 
-
 class Species(object):
 
     def __init__(self, id, name, evs=None):
         self.id = int(id)
         self.name = name
         self.evs = EvSet() if evs is None else evs
+        # print('\n\tSpecies init\n\t-----------')
+        # print(self)
 
     def __str__(self):
         return '#%03d %-10s %s' % (self.id, self.name, self.evs)
@@ -90,9 +103,17 @@ class Pokemon(object):
     @classmethod
     def from_dict(cls, dict):
         import pokedex
+        # print('\n\tfrom_dict\n\t-----------')
+        # print(dict)
         dict['species'] = pokedex.fetch_by_id(dict['species'])
         dict['evs'] = EvSet(**dict['evs'])
         return cls(**dict)
+
+    @staticmethod
+    def get_pokemon_by_id(ID):
+        import pokedex
+        return pokedex.fetch_by_id(ID)
+
 
     def __init__(self, id, species, name=None, item=None, pokerus=False, evs=None):
         self.id = int(id)
@@ -102,6 +123,8 @@ class Pokemon(object):
         self.item = item
         self.pokerus = pokerus
         self.evs = EvSet() if evs is None else evs
+        # print('\n\tPokemon init\n\t-----------')
+        # print(self)
 
     name = property(lambda self: self.get_name(),
                     lambda self, name: self.set_name(name))
@@ -121,17 +144,8 @@ class Pokemon(object):
             raise ValueError("Invalid item '%s'" % item)
         self._item = ITEMS[item] if item is not None else None
 
-    def __str__(self):
-        name = self.name
-        if self._name is not None:
-            name = '%s (%s)' % (name, self.species.name)
-        if self.id is None:
-            return name
-        else:
-            return '%d %s' % (self.id, name)
-
     def status(self):
-        status = [str(self)]
+        status = [str(self) + '\n']
         if self.pokerus:
             status.append('Pokerus')
         if self.item:
@@ -143,7 +157,7 @@ class Pokemon(object):
         padding = '* ' if self is active else '  '
         return '%s%s' % (padding, self)
 
-    def battle(species, number=1):
+    def battle(self, species, number, undo=False):
         '''
         Alter's a tracked Pokemons EVs to simulate having battled a Species.
         These values are altered by pokerus and any item held. The EV
@@ -154,10 +168,24 @@ class Pokemon(object):
             evs = self.item(evs)
         if self.pokerus:
             evs *= 2
-        self.evs += evs * number
+
+        if not undo:
+            self.evs += evs * number
+        else:
+            self.evs -= evs * number
+
+        return (evs * number)
 
     def to_dict(self):
         return {'species': self.species.id, 'name': self._name,
                 'pokerus': self.pokerus, 'item': self.item,
                 'evs': self.evs.to_dict(), 'id': self.id}
 
+    def __str__(self):
+        name = self.name
+        if self._name is not None:
+            name = '%s (%s)' % (name, self.species.name)
+        if self.id is None:
+            return name
+        else:
+            return '%d %s' % (self.id, name)
